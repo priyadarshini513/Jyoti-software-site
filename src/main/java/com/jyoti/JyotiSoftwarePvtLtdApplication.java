a5630313aa52b702b964400bc18c2165605dffe5
priyanka priyadarshini rout <pprout143@gmail.com>
2025-08-18 18:04:49 +0530

    Initial commit

---
+++ src/main/java/com/jyoti/JyotiSoftwarePvtLtdApplication.java
@@ -0,0 +1,13 @@
+package com.jyoti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyotiSoftwarePvtLtdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JyotiSoftwarePvtLtdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jyoti/JyotiSoftwarePvtLtdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyoti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JyotiSoftwarePvtLtdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
